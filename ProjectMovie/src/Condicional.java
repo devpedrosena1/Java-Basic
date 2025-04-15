7edb4ffc0d952b72690eb754a58529f81aa80d01
devpedrosena1 <devpedrosena1@gmail.com>
2025-04-15 20:17:59 -0300

    Continuando projeto do filme, estudando sobre condicionais e operadores lógicos

---
+++ ProjectMovie/src/Condicional.java
@@ -0,0 +1,23 @@
+public class Condicional {
+
+    public static void main(String[] args) {
+
+        int anoDeLancamento = 1990;
+        boolean incuidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento >= 2022) {
+            System.out.println("Filme recente.");
+        } else {
+            System.out.println("Filme antigo");
+        }
+
+        if (incuidoNoPlano == true || tipoPlano.equals("plus")) { // || -> OR, && -> AND
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Filme bloqueado.");
+        }
+
+    }
+}
